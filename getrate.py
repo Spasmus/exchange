12aad76242aab17de2bbd6d00d516c8a943c42fe
Spasmus <cesaraugustosoueu@gmail.com>
2020-04-28 11:53:53 -0300

    Update getrate.py

--- getrate.py
+++ getrate.py
@@ -8,19 +8,19 @@ timeout = 60.0 # Sixty seconds
 
 def ConsultaCotacao():
 
-		now = datetime.datetime.now()
-		print(str(now))
-		
-		#you can change currency, i am using usd only as example
-		resp = requests.get('https://economia.awesomeapi.com.br/usd/')
-		usdptax = 0
-		if resp.status_code != 200:
-		    # This means something went wrong.
-		    pass
-		for todo_item in resp.json():
+	now = datetime.datetime.now()
+	print(str(now))
+	
+	#you can change currency, i am using usd only as example
+	resp = requests.get('https://economia.awesomeapi.com.br/usd/')
+	usdptax = 0
+	if resp.status_code != 200:
+	    # This means something went wrong.
+	    pass
+	for todo_item in resp.json():
 
-		    print('{} {} {}'.format(todo_item['code'], todo_item['name'], todo_item['ask']))
-		    usdptax = todo_item['ask']				
+	    print('{} {} {}'.format(todo_item['code'], todo_item['name'], todo_item['ask']))
+	    usdptax = todo_item['ask']				
 	
 
 
